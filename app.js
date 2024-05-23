d5f04954b71e2b3b06771056e71b39f1f6aaff7d
taejun864 <taejun864@gmail.com>
2024-05-23 14:24:26 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.render('404', { pageTitle: 'Page not Found' });
+});
+
+app.listen(8080);
